76f8b79ffac68fe0f932828ea9bdfadb04bd2e22
Zeyum3 <loicminnoye@gmail.com>
2025-09-18 11:10:04 +0200

    Implémentation P1 assets sonore
    J'ai fait des prefabs de SoundCue pour les utiliser plus tard (au cas ou) sinon tous les sons actuels sont implémenter, si besoin de + je pourrais ajouter facilement

---
+++ Assets/Scripts/RandomAudioPlayer.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomAutoAudioCue : MonoBehaviour
+{
+    [SerializeField] private AudioSource source;
+    [SerializeField] private AudioClip[] clips;
+
+    [Header("Temps entre sons (aléatoire)")]
+    [SerializeField] private Vector2 intervalRange = new Vector2(10f, 20f);
+
+    [Header("Pitch & Volume random")]
+    [SerializeField] private Vector2 pitchRange = new Vector2(1f, 1f);
+    [SerializeField] private Vector2 volumeRange = new Vector2(1f, 1f);
+
+    private void Start()
+    {
+        StartCoroutine(RandomPlayer());
+    }
+
+    private IEnumerator RandomPlayer()
+    {
+        while (true)
+        {
+            // Attendre un temps aléatoire avant de jouer
+            float waitTime = Random.Range(intervalRange.x, intervalRange.y);
+            yield return new WaitForSeconds(waitTime);
+
+            PlayRandom();
+        }
+    }
+
+    private void PlayRandom()
+    {
+        if (clips.Length == 0) return;
+
+        AudioClip clip = clips[Random.Range(0, clips.Length)];
+        source.pitch = Random.Range(pitchRange.x, pitchRange.y);
+        source.volume = Random.Range(volumeRange.x, volumeRange.y);
+
+        source.PlayOneShot(clip);
+    }
+}
+
